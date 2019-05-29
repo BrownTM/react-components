8c53d73012f06cf8664bef64f763c6afc1722bf1
Therese Brown <browntm96@gmail.com>
2019-05-28 20:47:59 -0700

    Done rendering nested components

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// TODO
+var App = () => (
+  <div>
+    <h2>GroceryList</h2>
+    <GroceryList />
+  </div>
+);
+
+var GroceryList = () => (
+  <ul>
+    <li>Kale</li>
+    <li>Cucumbers</li>
+  </ul>
+);
+
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
